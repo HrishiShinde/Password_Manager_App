b4f59f445fb1668557fa2289c5f0e7c1e4ee371b
Hrishikesh Shinde <shindehrishikesh0115@gmail.com>
2021-01-31 21:58:03 +0530

    Login Design v1

--- app/src/main/java/com/pmapp/password_manager/SignUP.java
+++ app/src/main/java/com/pmapp/password_manager/SignUP.java
@@ -2,17 +2,13 @@ package com.pmapp.password_manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUP extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //Intent intent = new Intent(this, Login.class);
-        //startActivity(intent);
+        setContentView(R.layout.activity_sign_u_p);
     }
 }
