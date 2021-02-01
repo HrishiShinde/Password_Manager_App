003cac62524337fac8b8a5f0695c223a946781e8
Hrishikesh Shinde <shindehrishikesh0115@gmail.com>
2021-02-01 23:58:11 +0530

    Signup and login layout done!

--- app/src/main/java/com/pmapp/password_manager/Login.java
+++ app/src/main/java/com/pmapp/password_manager/Login.java
@@ -2,7 +2,9 @@ package com.pmapp.password_manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //getActionBar().hide();
+        //getActionBar().hide();
+    }
+    public void gotosign(View view){
+        Intent intent = new Intent(this, SignUP.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/pmapp/password_manager/SignUP.java
+++ app/src/main/java/com/pmapp/password_manager/SignUP.java
@@ -2,7 +2,9 @@ package com.pmapp.password_manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUP extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class SignUP extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_u_p);
+    }
+    public void gotolog(View view){
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
     }
 }
